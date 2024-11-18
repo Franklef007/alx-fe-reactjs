07df98888f4ab2a8ced73693bfa59f9e7eb1fb07
Franklef007 <131019463+Franklef007@users.noreply.github.com>
2024-11-18 02:02:58 +0100

    Add files via upload

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -10,26 +10,21 @@ import UserProfile from './components/UserProfile'
 import UserContext from './components/UserContext'
 
 function App() {
-  const [count, setCount] = useState( 0 )
-  const userData = {name:"Alice", age:"25", bio:"Loves hiking and photography"}
+  const [count, setCount] = useState(0)
+  const userData = { name: "Alice", age: "25", bio: "Loves hiking and photography" }
 
   return (
-    <>
+    <UserContext.Provider value={userData}>
       <div>
         <Header />
         <MainContent />
         <Footer />
-        <UserContext.Provider value={userData}>
-           <UserProfile />
-       </UserContext.Provider>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <UserProfile />
+        <WelcomeMessage />
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -38,11 +33,17 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <WelcomeMessage />
-    </>
+      <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </UserContext.Provider>
   )
 }
 
